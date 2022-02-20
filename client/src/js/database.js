805a158222221b0cbdca55d675fb135c1fc9dd4e
edytatar <edytatar@gmail.com>
2022-02-20 12:40:24 -0600

    Method that adds and gets content to and from IndexedDB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,39 @@
 import { openDB } from 'idb';
-
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB('text-editor', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains('text-editor')) {
+        console.log('Text Editor database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore('text-editor', { keyPath: 'id', autoIncrement: true });
+      console.log('Text Editor database created');
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-
+  // Method that takes some content and adds it to the IndexedDB database using the idb module
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+  const textEditorDb = await openDB('text-editor', 1);
+  const tx = textEditorDb.transaction('text-editor', 'readwrite');
+  const store = tx.objectStore('text-editor');
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
+  console.log(':rocket: - data saved to the database', result.value);
+};
+// Method that gets content from the IndexedDB database using the idb module
+export const getDb = async () => {
+  console.log('GET from the database');
+  const textEditorDb = await openDB('text-editor', 1);
+  const tx = textEditorDb.transaction('text-editor', 'readonly');
+  const store = tx.objectStore('text-editor');
+  const request = store.get(1);
+  const result = await request;
+  result
+    ? console.log(':rocket: - data retrieved from the database', result.value)
+    : console.log(':rocket: - data not found in the database');
+  
+    // Check if a variable is defined and if it is, return it
+  return result?.value;
+};
 initdb();
